6cfa22721dfc83f5b3990ef1f37f13b0c85a7789
Avinashkumar1307 <avinash2y20@gmail.com>
2024-06-28 09:42:11 +0530

    third

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import EcoSystem from "./EcoSystem";
+// import EcoSystem from "./EcoSystem";
 import Header from "./Header";
 import operation from './assets/operatingmodel.png'
 import demo from './assets/demo.png';
@@ -7,11 +7,17 @@ import eclipse1 from './assets/eclipse1.png'
 import top from './assets/top.png';
 import eclipse3 from './assets/ellipse3.png';
 import eclipse5 from './assets/ellipse5.png';
-import eclipse4 from './assets/ellipse4.png';
+import main from './assets/main.png'
+import { useState } from "react";
 export default function App() {
+  const [image, setImage] = useState(null);
+  const [imaging, setImaging] = useState(null)
+  const handleClick = (image) => {
+    setImage(image);
+  };
   return (
     <>
-      <div className="w-[1366px] h-[1024px] colorr relative overflow-hidden font-aktiv-grotesk">
+      {image == null ? <div className={`w-[1366px] h-[1024px] colorr relative overflow-hidden font-aktiv-grotesk `}>
         <Header />
         <div className="w-[634px] h-[31px] relative -top-[10px] left-[46px] z-10 text-[24px] text-[#FAFBFC]">
           The Unified Strategy Ecosystem brings together 3 worlds
@@ -25,24 +31,30 @@ export default function App() {
             Analyse the drivers and trends that influence the world of health to anticipate threats and opportunities to strategy execution.
           </div>
         </div>
-        <div className="w-[400.11px] h-[205.52px] absolute top-[732.65px] left-[56.89px] z-10">
-          <EcoSystem />
+        <div
+          className={`absolute z-50 w-[400.11px] h-[205.52px] top-[732.65px] left-[56.89px]`}
+          onClick={() => handleClick(main)}
+        >
+          <img
+            src={main}
+            alt='k'
+          />
         </div>
-        <div className="w-[107.39px] h-[50.77px] absolute  top-[822.75px] left-[428.12px]">
+        <div className="w-[107.39px] h-[50.77px] absolute  top-[822.75px] left-[428.12px] z-50">
           <p className="text-[16px] text-[#E0FBFF]">Health</p>
           <p className="text-[16px] text-[#FFFFFF]">Ecosystem</p>
         </div>
-        <div className="w-[85.91px] h-[50.77px] absolute text-[16px] top-[649.55px] left-[709.45px]">
+        <div className="w-[85.91px] h-[50.77px] absolute text-[16px] top-[649.55px] left-[709.45px] z-50">
           <p className="text-[#E0FBFF] ">Operating</p>
           <p className="text-white">Model</p>
         </div>
-        <div className="w-[136.77px] h-[161px] absolute  top-[558px] left-[588px]">
+        <div className="w-[136.77px] h-[161px] absolute  top-[558px] left-[588px] z-50" onClick={() => handleClick(operation)}>
           <img src={operation} alt="Operation" />
         </div>
-        <div className="w-[599.92px] h-[499px] absolute  top-[194px] left-[622px]">
+        <div className="w-[599.92px] h-[499px] absolute  top-[194px] left-[622px]" onClick={() => handleClick(demo)}>
           <img src={demo} alt="Demo" />
         </div>
-        <div className="modelCircle ">
+        <div className="modelCircle">
 
         </div>
         <div className="Eclipse ">
@@ -57,24 +69,41 @@ export default function App() {
         </div>
         <div id="eclipse1 text-white bg-red-900 z-30">
           <img src={eclipse1} alt="done" />
-          
+
+        </div>
+        <div className="leftone">
+          <img src={top} alt="left" />
+        </div>
+        <div className="eclipse3">
+          <img src={eclipse3} alt="d" />
+        </div>
+        <div className="eclipse5 z-50">
+          <img src={eclipse5} alt="d" />
+        </div>
+        <div className="eclipse5 z-50">
+          <img src={eclipse5} alt="d" />
+        </div>
+        <div className="eclipse5 z-50">
+          <img src={eclipse5} alt="d" />
         </div>
       </div>
-      <div className="leftone">
-        <img src={top} alt="left" />
-      </div>
-      <div className="eclipse3">
-        <img src={eclipse3} alt="d" />
-      </div>
-      <div className="eclipse5 z-50">
-        <img src={eclipse5} alt="d" />
-      </div>
-      <div className="eclipse5 z-50">
-        <img src={eclipse5} alt="d" />
-      </div>
-      <div className="eclipse4 z-50">
-        <img src={eclipse4} alt="d" />
-      </div>
+        :
+        <div className={`flex flex-col justify-center items-center w-[1366px] h-[1024px] colorr relative overflow-hidden font-aktiv-grotesk `} onClick={() => (setImage(null))}>
+          <div className={`w-[1366px] h-[102px] relative overflow-hidden font-aktiv-grotesk `}>
+          <Header />
+          </div>
+          <div className="h-[922px] flex justify-center items-center">
+          <img
+            src={image}
+            alt="main"
+            className={`${image === main ? 'w-[1000px]' :
+                image === demo ? 'w-[800px]' :
+                  image === operation ? 'w-[600px]' :
+                    ''
+              }`}
+          />
+          </div>
+        </div>}
 
     </>
   )
--- src/EcoSystem.jsx
+++ src/EcoSystem.jsx
@@ -1,33 +1,7 @@
 import React from 'react';
-import work from './assets/work.png';
-import dura from './assets/dura.png';
-import rese from './assets/rese.png';
-import pharm from './assets/pharm.png';
-import it from './assets/it.png';
-import insu from './assets/insu.png';
-import policy from './assets/policy.png';
-import health from './assets/health.png';
-import phy from './assets/phy.png';
-import edip from './assets/edip.png';
-import upper from './assets/upper.png';
-import midup from './assets/midup.png';
-import mid from './assets/mid.png';
-import lower from './assets/lower.png'
+import main from './assets/main.png'
 export default function EcoSystem() {
     return (
-        <div className="relative z-50">
-            <div className='w-[272.38px] h-[27.84px] absolute top-[9.36px] left-[64px] z-50'>
-                <img src={upper} alt='Upper' />
-            </div>
-            <div className='w-[208px] h-[84.11px] absolute top-[13.01px] left-[98.6px]' >
-                <img src={midup} alt='nothing'/>
-            </div>
-            <div className='w-[41.65px] h-[22.59px] absolute top-[92.77px] left-[179.54px]' >
-                <img src={mid} alt='nothing'/>
-            </div>
-            <div className='w-[337.41px] h-[91.83px] absolute top-[113.17px] left-[31.48px]' >
-                <img src={lower} alt='nothing'/>
-            </div>
-        </div>
+      <></>
     );
 }
